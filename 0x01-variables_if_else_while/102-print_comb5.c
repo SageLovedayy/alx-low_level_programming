ae4ae1a58e83a79e256c43936d1fe0000809e857
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-09-08 17:53:32 -0700

    Correct Script 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,60 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - Ebtry point
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int tens1, ones1, tens2, ones2, tens3, ones3, tens4, ones4;
+	int a, b;
 
-	for (tens1 = 0; tens1 <= 9; tens1++)
+	for (a = 0; a < 100; a++)
 	{
-		for (ones1 = 0; ones1 <= 9; ones1++)
+		for (b = 0; b < 100; b++)
 		{
-			for (tens2 = 0; tens2 <= 9; tens2++)
+			if (a < b)
 			{
-				for (ones2 = 0; ones2 <= 9; ones2++)
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a != 98 || b != 99)
 				{
-					if (tens2 < tens1 || (tens2 == tens1 && ones2 <= ones1))
-					{
-						continue;
-					}
-
-								{
-					putchar(tens1 + '0');
-					putchar(ones1 + '0');
+					putchar(',');
 					putchar(' ');
-					putchar(tens2 + '0');
-					putchar(ones2 + '0');
-
-					if (!(tens1 == 9 && ones1 == 9 && tens2 == 9 && ones2 == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-
-					for (tens3 = 0; tens3 <= 9; tens3++)
-					{
-						for (ones3 = 0; ones3 <= 9; ones3++)
-						{
-							if (tens3 < tens2 || (tens3 == tens2 && ones3 <= ones2))
-							{
-								continue;
-							}
-
-							putchar(tens1 + '0');
-							putchar(ones1 + '0');
-							putchar(' ');
-							putchar(tens2 + '0');
-							putchar(ones2 + '0');
-							putchar(tens3 + '0');
-							putchar(ones3 = '0');
 				}
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
