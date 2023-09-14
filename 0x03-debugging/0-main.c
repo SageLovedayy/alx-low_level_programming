3d0b5790512934a7baffe2c5ebcc1ccbd9cf85d3
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-09-14 09:35:40 -0700

    Create c files for debugging and testing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  main - tests function that prints whether or not an integer is positive
+ *  Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
