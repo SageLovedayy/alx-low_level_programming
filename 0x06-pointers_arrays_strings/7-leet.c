9f358e83e4b103b0e6f67a8418cebe4ad35caab3
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-09-20 14:16:42 -0700

    Modify

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,38 +1,25 @@
-#include <stdio.h>
-#include <string.h>
-
-char *leet(char *str)
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
 {
-	char *ptr = str; // Pointer to the input string
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	while (*ptr != '\0') {
-		// Check if the current character is an 'a' or 'A'
-		// and replace it with '4' if true
-		if (*ptr == 'a' || *ptr == 'A') {
-			*ptr = '4';
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
 		}
-		// Check if the current character is an 'e' or 'E'
-		// and replace it with '3' if true
-		else if (*ptr == 'e' || *ptr == 'E') {
-			*ptr = '3';
-		}
-		// Check if the current character is an 'o' or 'O'
-		// and replace it with '0' if true
-		else if (*ptr == 'o' || *ptr == 'O') {
-			*ptr = '0';
-		}
-		// Check if the current character is a 't' or 'T'
-		// and replace it with '7' if true
-		else if (*ptr == 't' || *ptr == 'T') {
-			*ptr = '7';
-		}
-		// Check if the current character is an 'l' or 'L'
-		// and replace it with '1' if true
-		else if (*ptr == 'l' || *ptr == 'L') {
-			*ptr = '1';
-		}
-
-		ptr++; // Move to the next character
 	}
+	return (n);
+}
 
-	return (str);
