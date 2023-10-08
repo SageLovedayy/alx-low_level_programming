b28d2ca4d904c98ae82191a994dc575218077e3e
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-08 18:09:08 +0100

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,81 +1,92 @@
 #include "main.h"
-#include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define ERR_MSG "Error"
 
 /**
-* is_valid_number - checks if argument is not a number
-* @str: input string
-* Return: 0 if digit, 1 otherwise
-*/
-int is_valid_number(char *str)
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
 {
-	while (*str)
-	{
-		if (*str < '0' || *str > '9')
-			return (0);
+	int i = 0;
 
-		str++;
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
 	return (1);
 }
 
 /**
-* mystrlen - returns the length of a string
-* @str: string
-* Return: string length
-*/
-int mystrlen(char *str)
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (*str)
+	while (s[i] != '\0')
 	{
 		i++;
-		str++;
 	}
 	return (i);
 }
 
 /**
-* multiply_numbers - multiplies two integers
-* @num1: integer
-* @num2: integer
-*/
-void multiply_numbers(char *num1, char *num2)
+ * errors - handles errors for main
+ */
+void errors(void)
 {
-	int len1, len2, total_len, i, a, *result, digit1, digit2, carry;
+	printf("Error\n");
+	exit(98);
+}
 
-	len1 = mystrlen(num1);
-	len2 = mystrlen(num2);
-	total_len = len1 + len2 + 1;
-	result = malloc((total_len) * sizeof(int));
-	if (result == NULL)
-		exit(1);
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
 	for (i = 0; i <= len1 + len2; i++)
-	{
 		result[i] = 0;
-	}
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = num1[len1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = mystrlen(num2) - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = num2[len2] - '0';
+			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-		{
 			result[len1 + len2 + 1] += carry;
-		}
 	}
-	for (i = 0; i < total_len - 1; i++)
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
@@ -86,30 +97,5 @@ void multiply_numbers(char *num1, char *num2)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
-}
-
-
-/**
-* main - Uses multiply_numbers function on two arguments
-* @argc: number of arguments
-* @argv:  argument vectors
-* Return: 0 on success, 98 on failure
-*
-*/
-int main(int argc, char *argv[])
-{
-
-	char *num1, *num2;
-
-	num1 = argv[1];
-	num2 = argv[2];
-
-	if (argc != 3 || !is_valid_number(num1) || !is_valid_number(num2))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	multiply_numbers(num1, num2);
 	return (0);
 }
