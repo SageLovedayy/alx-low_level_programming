ab1c0eaf8ed7a4a41adf0e350be949eafb539d23
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-08 17:30:05 +0100

    .

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,6 +3,11 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+/**
+* is_valid_number - checks if argument is not a number
+* @str: input string
+* Return: 0 if digit, 1 otherwise
+*/
 int is_valid_number(char *str)
 {
 	while (*str)
@@ -17,9 +22,15 @@ int is_valid_number(char *str)
 	return (1);
 }
 
-int mystrlen (char *str)
+/**
+* mystrlen - returns the length of a string
+* @str: string
+* Return: string length
+*/
+int mystrlen(char *str)
 {
 	int i;
+
 	i = 0;
 	while (*str)
 	{
@@ -29,52 +40,29 @@ int mystrlen (char *str)
 	return (i);
 }
 
-int main(int argc, char *argv[])
+/**
+* multiply_numbers - multiplies two integers
+* @num1: integer
+* @num2: integer
+*/
+void multiply_numbers(char *num1, char *num2)
 {
-	int len1, len2, total_len, i, a;
-	int *result, digit1, digit2, carry;
-	char *num1, *num2;
+	int len1, len2, total_len, i, a, *result, digit1, digit2, carry;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	if (!is_valid_number(argv[1]) || !is_valid_number(argv[2]))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	/**
-	* num1 = atoi(argv[1]);
-	* num2 = atoi(argv[2]);
-	* result = (num1 * num2);
-	*/
-
-	num1 = argv[1];
-	num2 = argv[2];
 	len1 = mystrlen(num1);
 	len2 = mystrlen(num2);
 	total_len = len1 + len2;
-
 	result = malloc((total_len + 1) * sizeof(int));
 	if (result == NULL)
-	{
 		exit(1);
-	}
-
 	for (i = 0; i <= total_len; i++)
 	{
 		result[i] = 0;
 	}
-
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = num1[len1] - '0';
 		carry = 0;
-
 		for (len2 = mystrlen(num2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = num2[len2] - '0';
@@ -82,32 +70,41 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
-
 		if (carry > 0)
 		{
 			result[len1 + len2 + 1] += carry;
 		}
 	}
-
 	for (i = 0; i < total_len; i++)
 	{
 		if (result[i])
-		{
 			a = 1;
-		}
 		if (a)
-		{
 			_putchar(result[i] + '0');
-		}
 	}
-
 	if (!a)
-	{
 		_putchar('0');
-	}
-
 	_putchar('\n');
 	free(result);
+}
 
+
+/**
+* main - Uses multiply_numbers function on two arguments
+* @argc: number of arguments
+* @argv:  argument vectors
+* Return: 0 on success, 98 on failure
+*
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 3 || !is_valid_number(argv[1]) || !is_valid_number(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+
+	multiply_numbers(argv[1], argv[2]);
 	return (0);
 }
