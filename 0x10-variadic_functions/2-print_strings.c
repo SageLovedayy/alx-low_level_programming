db3241e3ca6fa334039a5af88e42a5b4b547d123
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-12 22:17:17 +0100

    Add variadic functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_strings - prints strings, followed by a new line
+* @separator: string seperator
+* @n: number of strings passed
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *ptr;
+
+	va_list ap;
+	va_start(ap, n);
+
+
+	for (i = 0; i < n; i++)
+	{
+		ptr = va_arg(ap, char *);
+
+		if (ptr != NULL)
+			printf("%s", ptr);
+		else
+			printf("(nil)");
+
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
+
+	}
+
+	printf("\n");
+
+	va_end(ap);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,8 +2,12 @@
 #define VARIADIC_FUNCTIONS_H
 
 int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_char(char value, const char *seperator);
 void print_integer(int value, const char *seperator);
