b0b712d3564c2e5a1f3a60216a721b597e4a72f8
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-25 00:10:04 +0100

    mk

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,43 +9,17 @@ size_t print_listint_safe(const listint_t *head)
 {
 	/* USING FLOYD'S CYCLE FINDING ALGORITHM*/
 
-	const listint_t *slow, *fast, *temp;
-	size_t nodeCount = 0;
-	int loopDetected = 0;
-	int loopNodeCount = 0;
+	size_t pichu = 0;
+	const listint_t *aux_node = head;
 
-	slow = fast = head;
-
-	while (slow != NULL && fast != NULL && fast->next != NULL)
-	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		nodeCount++;
-
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
-		{
-			loopDetected = 1;
-			break;
-		}
-	}
-
-	/*Detect loop and print associated nodes*/
-	if (loopDetected)
-	{
-		temp = slow;
-		
-		do {
-			printf("[%p] %d\n", (void *)temp, temp->n);
-			temp = temp->next;
-			loopNodeCount++;
-		} while (temp != slow);
-
-		printf("-> [%p] %d\n", (void *)temp, temp->n);
-		nodeCount += loopNodeCount; 
+	if (!head)
 		exit(98);
-	}
 
-	return (nodeCount);
+	while (aux_node)
+	{
+		printf("[%p] %i\n", (void *)aux_node, aux_node->n);
+		aux_node = aux_node->next;
+		pichu++;
+	}
+	return (pichu);
 }
