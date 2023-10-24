153aed446ee2d322bbc657ca684b708cb6a9b026
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-24 12:04:13 +0100

    Mod 24/10/23 -03

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,25 +8,12 @@
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *newNode;
-
-	if (n == NULL)
-		return (NULL);
-
-	newNode = malloc(sizeof(listint_t));
+	listint_t *newNode = malloc(sizeof(listint_t));
 
 	if (newNode == NULL)
 		return (NULL);
 
-
 	newNode->n = n;
-
-	if (newNode->n == NULL)
-	{
-		free(newNode);
-		return (NULL);
-	}
-
 	newNode->next = *head;
 	*head = newNode;
 
