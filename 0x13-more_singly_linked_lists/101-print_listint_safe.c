e2213e9a13482f51374e11ad522fea4b80dd6192
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-25 01:50:44 +0100

    25-10-23-01

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,17 +7,16 @@
  */
 size_t caculate_loop_length(const listint_t *head)
 {
+	/* USING FLOYD'S CYCLE FINDING ALGORITHM */
 	const listint_t *slow_ptr, *fast_ptr;
-	size_t nodes_count = 1;
+	size_t nodeCount = 1;
 
 	/* Check for empty list or list with only one node */
 	if (head == NULL || head->next == NULL)
-		return 0;
-
+		return (0);
 	/* Initialize slow and fast pointers */
 	slow_ptr = head->next;
 	fast_ptr = (head->next)->next;
-
 	/* Detect loop using Floyd's cycle-finding algorithm */
 	while (fast_ptr)
 	{
@@ -27,31 +26,27 @@ size_t caculate_loop_length(const listint_t *head)
 			slow_ptr = head;
 			while (slow_ptr != fast_ptr)
 			{
-				nodes_count++;
+				nodeCount++;
 				slow_ptr = slow_ptr->next;
 				fast_ptr = fast_ptr->next;
 			}
-
 			/* Move slow pointer until it reaches the start of the loop */
 			slow_ptr = slow_ptr->next;
-
 			/* Count nodes in the loop */
 			while (slow_ptr != fast_ptr)
 			{
-				nodes_count++;
+				nodeCount++;
 				slow_ptr = slow_ptr->next;
 			}
 
-			return nodes_count;
+			return (nodeCount);
 		}
-
 		/* Move slow pointer one step and fast pointer two steps */
 		slow_ptr = slow_ptr->next;
 		fast_ptr = (fast_ptr->next)->next;
 	}
-
 	/* No loop detected */
-	return 0;
+	return (0);
 }
 
 /**
@@ -61,15 +56,15 @@ size_t caculate_loop_length(const listint_t *head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes_count, index = 0;
+	size_t nodeCount, index = 0;
 
 	/* Calculate the number of nodes in the looped list */
-	nodes_count = caculate_loop_length(head);
+	nodeCount = caculate_loop_length(head);
 
 	/* Loop detected, print nodes in the loop */
-	if (nodes_count != 0)
+	if (nodeCount != 0)
 	{
-		for (index = 0; index < nodes_count; index++)
+		for (index = 0; index < nodeCount; index++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
@@ -80,12 +75,12 @@ size_t print_listint_safe(const listint_t *head)
 	/* No loop detected, print all nodes in the list */
 	else
 	{
-		for (; head != NULL; nodes_count++)
+		for (; head != NULL; nodeCount++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
 	}
 
-	return nodes_count;
+	return (nodeCount);
 }
