b7583e7c6e77f1ebe7894335f2b08db67138c574
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-26 23:56:30 +0100

    mod 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,16 +13,22 @@ unsigned int binary_to_uint(const char *b)
 	int power = 1;
 	int i;
 
+	if (b == NULL)
+		return (0);
+
 	while (b[count] != '\0')
-	{
-		count++;
-	}
-
-
-	for (i = count - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
+
+		count++;
+	}
+
+	if (count == 0)
+		return (0);
+
+	for (i = count - 1; i >= 0; i--)
+	{
 
 		if (b[i] == '1')
 			result += power;
