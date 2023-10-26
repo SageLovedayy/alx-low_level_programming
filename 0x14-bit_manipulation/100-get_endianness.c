efeb90d97311ec6c036daccc55c8aeeee6f03f42
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-26 23:24:49 +0100

    Add advanced

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* get_endianness - determine endianness of underlying system architecture
+* Return: 0 if big endian, 1 if little endian
+*/
+int get_endianness(void)
+{
+	int num = 1;
+
+	char *bytePtr = (char *)&num;
+
+	return (*bytePtr == 1);
+}
