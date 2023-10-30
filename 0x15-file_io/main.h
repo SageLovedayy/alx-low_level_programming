2896308ffd8602dc7b7fa705eb1e077e5aeeca75
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-10-31 00:59:35 +0100

    Mod 1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <elf.h>
 
 #define BUFFER_SIZE 1024
 
@@ -19,5 +20,8 @@ int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int copyFiles(const char *file_from, const char *file_to);
+void handleError(int error_code, const char *file_name);
+int main(int argc, char *argv[]);
 
 #endif /*main.h*/
