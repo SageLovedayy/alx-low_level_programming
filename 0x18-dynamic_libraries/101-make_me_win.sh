d4bcaa4932b50c5dd705ab2cb5cda57c6c6d25cb
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2023-12-12 02:25:36 +0100

    Mod ad

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -qP /tmp/ https://github.com/SageLovedayy/alx-low_level_programming/raw/master/0x18-dynamic_libraries/hackmole.so
-export LD_PRELOAD="/tmp/mymole.so"
+export LD_PRELOAD=./hackmole.so
