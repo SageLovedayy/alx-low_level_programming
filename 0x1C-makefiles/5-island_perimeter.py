ac63fca838b5a09f7cb92ce978336c8517d16bb9
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-02-22 00:01:39 +0100

    Add island perimeter module

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,9 +16,11 @@ def island_perimeter(grid):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:  # Check if cell has neighbor to the top
+                # Check if cell has neighbor to the top
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:  # Check if cell has neighbor to the left
+                # Check if cell has neighbor to the left
+                if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
 
     return perimeter
